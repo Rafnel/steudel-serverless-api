2a7592c38b583d22f3c1ec808e6e029db54c89b6
Rafnel <zacshack174@gmail.com>
2019-07-28 19:44:43 -0500

    initial backend get and create swim components

---
+++ createSwimComponent.js
@@ -0,0 +1,28 @@
+import uuid from "uuid";
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+export async function main(event, context) {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: "swim_components_table",
+    Item: {
+      username: data.username,
+      component_id: uuid.v1(),
+      component_body: data.component_body,
+      difficulty: data.difficulty,
+      date_created: new Date().toLocaleString("en-US", {timeZone: "America/Denver"}),
+      intervals: data.intervals,
+      sett: data.sett,
+      tags: data.tags,
+      yardage: data.yardage
+    }
+  };
+
+  try {
+    await dynamoDbLib.call("put", params);
+    return success(params.Item);
+  } catch (e) {
+    return failure({ status: false });
+  }
+}
