63c621edd365eefd2dd8b6baa2324cd5160947d2
Rafnel <zacshack174@gmail.com>
2019-08-10 22:56:33 -0500

    create a swim workout

---
+++ createSwimWorkout.js
@@ -0,0 +1,29 @@
+import uuid from "uuid";
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+export async function main(event, context) {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: "swim_workout_table",
+    Item: {
+      username: data.username,
+      workout_id: uuid.v1(),
+      warmup: data.warmup,
+      preset: data.preset,
+      mainset: data.mainset,
+      cooldown: data.cooldown,
+      difficulty: data.difficulty,
+      date_created: new Date().toLocaleString("en-US", {timeZone: "America/Denver"}).toString(),
+      yardage: data.yardage,
+      likes: 0
+    }
+  };
+
+  try {
+    await dynamoDbLib.call("put", params);
+    return success(params.Item);
+  } catch (e) {
+    return failure({ status: false });
+  }
+}
