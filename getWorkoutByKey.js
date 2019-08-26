f89c740e5154926b3ef6da720405bbdb26b78ebf
Zachary Steudel <zacshack174@gmail.com>
2019-08-25 23:30:07 -0500

    added getting workout by key

---
+++ getWorkoutByKey.js
@@ -0,0 +1,25 @@
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+export async function main(event, context) {
+  const params = {
+    TableName: "swim_workout_table",
+    // 'Key' defines the partition key and sort key of the item to be retrieved
+    Key: {
+      username: event.pathParameters.username,
+      workout_id: event.pathParameters.workout_id
+    }
+  };
+
+  try {
+    const result = await dynamoDbLib.call("get", params);
+    if (result.Item) {
+      // Return the retrieved item
+      return success(result.Item);
+    } else {
+      return failure({ status: false, error: "Item not found." });
+    }
+  } catch (e) {
+    return failure({ status: false });
+  }
+}
